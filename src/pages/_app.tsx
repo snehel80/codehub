51e4b0f2e4b7737e002590b3575f76773370ba8b
snehel <slsf3939@gmail.com>
2023-06-18 12:32:37 +0530

    tab bar changed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+        <Head>
+            <title>Code Hub</title>
+            <meta name="viewport" content="width-device-width, initial-scale=1" />
+            <link rel="icon" href="/codehub.png" />
+            <meta name="description" content="web applications that contains leetcode problems and video solutions" />
+        </Head>
+        <Component {...pageProps} />
+    </>
+  )
 }
